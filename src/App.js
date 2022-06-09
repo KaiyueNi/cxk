79c1bd9b6a5e4ec12fdd45c1bbd7aa956fc10738
KaiyueNi <1900022817@pku.edu.cn>
2022-06-09 22:22:05 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render () {
-    return <h2>Hello App test change23</h2>
+    return <h2>test github</h2>
   }
 }
