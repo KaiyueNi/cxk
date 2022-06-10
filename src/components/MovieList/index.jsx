7edaba6b0b28da55d212e10f8e768b9da1d56ad2
KaiyueNi <1900022817@pku.edu.cn>
2022-06-11 01:44:06 +0800

    修改电影列表排版

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,20 +1,77 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { EditOutlined, SmileOutlined, HeartOutlined, HeartFilled } from '@ant-design/icons';
+import { Card, Rate } from 'antd';
 import './index.scss';
 
 const { Meta } = Card;
+const cardlist = [
+  {
+    title: '神雕侠侣',
+    data: 4.5,
+    like: 1
+  },
+  {
+    title: '鹿鼎记',
+    data: 3.5,
+    like: 0
+  },
+  {
+    title: '大话西游',
+    data: 4.0,
+    like: 1
+  },
+  {
+    title: '老友记',
+    data: 4.5,
+    like: 0
+  },
+  {
+    title: '吸血鬼日记',
+    data: 3.5,
+    like: 1
+  },
+  {
+    title: '红高粱',
+    data: 4.0,
+    like: 1
+  },
+  {
+    title: '老友记',
+    data: 4.5,
+    like: 0
+  },
+  {
+    title: '吸血鬼日记',
+    data: 3.5,
+    like: 1
+  }
+];
 
 export default class index extends Component {
   render () {
     return (
-      <div>
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-        >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
+      <div className='cardlist'>
+        {cardlist.map((item, index) => (
+          <Card
+            key={index}
+            hoverable
+            className='oneCard'
+            // actions={[
+            //   <HeartOutlined key="like" />,
+            //   <SmileOutlined  key="ellipsis"/>
+            // ]}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          >
+            <Meta title={item.title} />
+            <Rate disabled allowHalf defaultValue={item.data} />
+            <span className="ant-rate-text">{item.data}</span>
+            {item.like ? <HeartOutlined />
+              : <HeartFilled className='like' />}
+            {/* <SmileOutlined  key="ellipsis"/> */}
+          </Card>
+        )
+        )}
+        <i></i><i></i><i></i><i></i><i></i>
       </div>
     )
   }
